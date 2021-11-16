13fe2df389e586a3855180e8bd2ffec65c802108
chrisweaver1 <chrisweaver1@pm.me>
2021-11-16 21:59:07 +0000

    Add and configure options

--- src/Oak.Events/Configure.cs
+++ src/Oak.Events/Configure.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Oak.Events.Implementations;
@@ -6,32 +7,37 @@ namespace Oak.Events
 {
     public static class Configure
     {
-        public static void AddOakEventDispatcher(this IServiceCollection services)
+        public static void AddOakEventDispatcher(
+            this IServiceCollection services, 
+            Action<EventDispatcherOptions> options)
         {
-            services.TryAddScoped<IEventDispatcher, AmbiguousEventDispatcher>();
+            services.AddScoped<IEventDispatcher, DefaultEventDispatcher>();
+            services.AddScoped<IEventDispatcher, AmbiguousEventDispatcher>();
+            services.AddScoped<IEventDispatcher, IndependentEventDispatcher>();
+            services.Configure<EventDispatcherOptions>(options);
         }
 
-        public static void AddEvent<TEventHandler, TEvent>(this IServiceCollection services) where TEvent : IEvent where TEventHandler : class, IEventHandler<TEvent>
+        public static void AddEvent<TEventHandler, TEvent>(this IServiceCollection services) 
+            where TEvent : IEvent where TEventHandler : class, IEventHandler<TEvent>
         {
-            services.AddOakEventDispatcher();
             services.AddTransient<IEventHandler<TEvent>, TEventHandler>();
         }
 
-        public static void AddAsyncEvent<TEventHandler, TEvent>(this IServiceCollection services) where TEvent : IEvent where TEventHandler : class, IAsyncEventHandler<TEvent>
+        public static void AddAsyncEvent<TEventHandler, TEvent>(this IServiceCollection services) 
+            where TEvent : IEvent where TEventHandler : class, IAsyncEventHandler<TEvent>
         {
-            services.AddOakEventDispatcher();
             services.AddTransient<IAsyncEventHandler<TEvent>, TEventHandler>();
         }
 
-        public static void TryAddEvent<TEventHandler, TEvent>(this IServiceCollection services) where TEvent : IEvent where TEventHandler : class, IEventHandler<TEvent>
+        public static void TryAddEvent<TEventHandler, TEvent>(this IServiceCollection services) 
+            where TEvent : IEvent where TEventHandler : class, IEventHandler<TEvent>
         {
-            services.AddOakEventDispatcher();
             services.TryAddTransient<IEventHandler<TEvent>, TEventHandler>();
         }
 
-        public static void TryAddAsyncEvent<TEventHandler, TEvent>(this IServiceCollection services) where TEvent : IEvent where TEventHandler : class, IAsyncEventHandler<TEvent>
+        public static void TryAddAsyncEvent<TEventHandler, TEvent>(this IServiceCollection services) 
+            where TEvent : IEvent where TEventHandler : class, IAsyncEventHandler<TEvent>
         {
-            services.AddOakEventDispatcher();
             services.TryAddTransient<IAsyncEventHandler<TEvent>, TEventHandler>();
         }
     }
